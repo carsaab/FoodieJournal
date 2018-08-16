216d87717ff949dbd9fc5ddb269188719c3e02d8
Caroline Saab <caroline.saab@learninga-z.com>
2018-08-16 11:23:12 -0400

    refactored \login page using separate function to handle login request

--- login.php
+++ login.php
@@ -1,10 +1,10 @@
-<?php error_reporting(E_ALL);
+<?php
+    require_once("handle-login-request.inc");
+    require_once("foodieJournalCrudClasses.inc");
     session_start();
-    $db = mysqli_connect("localhost", "ldbuser", $_ENV["DB_PASSWORD"], "trainingproject");
-
     if(isset($_SESSION["userId"])){
         echo "<script>location.href='\index.php'</script>";
-}
+    }
 ?>
 
 <!DOCTYPE html>
@@ -15,33 +15,15 @@
 
 <body>
 <form method="post" action="" name="Login">
-    Email <input type="text" name="email"> <br><br>
+    Username <input type="text" name="username"> <br><br>
     Password <input type="password" name="password"> <br><br>
     <input type="submit" name="submit" value="Login">
 </form>
 
-<?php
-    if (isset($_POST["submit"])){
-        $email = $_POST["email"];
+<br>
+<?php handleLoginRequest(); ?>
+<br><br>
 
-        $query = sprintf("SELECT user_id FROM users WHERE email='%s'", $email); //TODO check for sha3(password)
-        $result = mysqli_query($db, $query);
-        $numUsersFound = mysqli_num_rows($result);
-        if ($numUsersFound != 1){
-            echo "Email Address and/or Password incorrect. Please try again.";
-        }
-        else{
-            $userId = mysqli_fetch_array($result)["user_id"];
-            echo "userId" . $userId . "<br>"; //DEBUG
-            echo "query" . $query . "<br>";
-            $_SESSION["userId"] = $userId;
-            mysqli_close($db);
-            echo "<script>location.href='\index.php'</script>";
-        }
-    }
-
-    mysqli_close($db);
-?>
 Don't have an account? <a href="\create_user.php">Register now for free!</a>
 </body>
 </html>
